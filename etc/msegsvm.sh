436ea93331b1599d83e78c528d1b899dd4ff4072
1maginat0r <imaginai90@proton.me>
2022-02-25 15:11:03 +0000

    add clean task

--- etc/msegsvm.sh
+++ etc/msegsvm.sh
@@ -3,3 +3,8 @@
 gdal_polygonize.py raster.ers -f "ESRI Shapefile" polygons.shp
 mv stats.test toposys2.test
 mv training.train toposys2.train
+./easy.py toposys2.train toposys2.test
+rm toposys2.test.scale
+rm toposys2.train.range
+rm toposys2.train.scale
+rm toposys2.train.scale.out
