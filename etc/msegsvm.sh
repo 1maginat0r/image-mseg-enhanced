4855203f4d8db95942421388ab6ea5bd6e15f95d
1maginat0r <imaginai90@proton.me>
2022-02-25 21:27:58 +0000

    Fixed capitalization.

--- etc/msegsvm.sh
+++ etc/msegsvm.sh
@@ -10,3 +10,4 @@ rm toposys2.train.scale
 rm toposys2.train.scale.out
 ./obia-class-export-svm raster TTAMask toposys2.test toposys2.test.predict classification classraster
 gdal_polygonize.py classraster.ers -f "ESRI Shapefile" classes.shp
+./obia-utils-confmatrix classraster truth 4 474 473 > confmatrix.txt
